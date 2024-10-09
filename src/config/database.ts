51edb93d6c3ad48051b55cf5eace2d1d2809e2cc
Guilherme Cavalcante <150976331+GuiCavalcanteDev@users.noreply.github.com>
2024-10-09 19:54:45 -0300

    v2

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = '';
+const connectionString = 'postgresql://postgres:ETJkyhudDofAZdzdpSitjJLHhwjjJUmN@junction.proxy.rlwy.net:31278/railway';
 
 const pool = new Pool({
   connectionString,
